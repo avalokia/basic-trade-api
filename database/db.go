c337eec01323e71baea526566ed0c10cb8f392f2
Ra <82923488+avalokia@users.noreply.github.com>
2023-09-18 19:03:02 +0700

    Add reconnecting DB in GetDB

--- database/db.go
+++ database/db.go
@@ -20,7 +20,7 @@ func StartDB() {
 	// err := godotenv.Load()
 	// if err != nil {
 	// 	log.Fatalln("Error loading .env file:", err)
-	// 	return err
+	// 	// return err
 	// }
 	// Get DB credentials from .env file
 	host := os.Getenv("HOST")
@@ -31,13 +31,17 @@ func StartDB() {
 	// Define connection string
 	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai", host, user, password, dbname, dbport)
 	// Open connection with postgreSQL
+	fmt.Println("dsn:", dsn)
+	fmt.Println("Opening connection with PSQL...")
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		log.Fatalln("Failed connecting DB:", err)
+		fmt.Println("Failed connecting DB:", err)
+		// log.Fatalln("Failed connecting DB:", err)
 	}
 
 	// Auto migration
-	err = db.Debug().AutoMigrate(models.Admin{}, models.Product{}, models.Variant{})
+	fmt.Println("Auto migrating DB...")
+	err = db.AutoMigrate(models.Admin{}, models.Product{}, models.Variant{})
 	if err != nil {
 		log.Fatalln("Failed to auto migrate:", err)
 	}
@@ -46,8 +50,27 @@ func StartDB() {
 // Get DB that has been started by StartDB()
 func GetDB() *gorm.DB {
 	// If db hasn't been started, start db
-	// if db == nil {
-	// 	StartDB()
-	// }
+	if db == nil {
+		fmt.Println("DB is nil. Starting DB..")
+		StartDB()
+		// RECONNECT DB
+		// Get DB credentials from .env file
+		host := os.Getenv("HOST")
+		user := os.Getenv("DB_USER")
+		password := os.Getenv("DB_PASSWORD")
+		dbname := os.Getenv("DB_NAME")
+		dbport := os.Getenv("DB_PORT")
+		// Define connection string
+		dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Shanghai", host, user, password, dbname, dbport)
+		// Open connection with postgreSQL
+		fmt.Println("dsn:", dsn)
+		fmt.Println("Opening connection with PSQL...")
+		db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if err != nil {
+			fmt.Println("Failed connecting DB:", err)
+			// log.Fatalln("Failed connecting DB:", err)
+		}
+		return db
+	}
 	return db
 }
